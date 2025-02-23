23563c05ad0cdc15917a8dba8c9027299aaa7665
Simon Cornelius <simonjoecornelius@gmail.com>
2025-02-23 13:59:05 +0100

    feat: raw noise as scrolling ground!!

--- src/screen_gameplay.c
+++ src/screen_gameplay.c
@@ -23,6 +23,7 @@
 *
 **********************************************************************************************/
 
+#include "stdlib.h"
 #include "raylib.h"
 #include "screens.h"
 
@@ -31,6 +32,11 @@
 //----------------------------------------------------------------------------------
 static int framesCounter = 0;
 static int finishScreen = 0;
+int noiseWidth = 0;
+int noiseHeight = 250;
+int noiseY = 0;
+Image perlinImage;
+Texture2D perlinTex;
 
 //----------------------------------------------------------------------------------
 // Gameplay Screen Functions Definition
@@ -39,38 +45,40 @@ static int finishScreen = 0;
 // Gameplay Screen Initialization logic
 void InitGameplayScreen(void)
 {
-    // TODO: Initialize GAMEPLAY screen variables here!
     framesCounter = 0;
     finishScreen = 0;
+    noiseWidth = GetScreenWidth();
+    noiseY = GetScreenHeight() - noiseHeight;
+    perlinImage = GenImagePerlinNoise(noiseWidth, noiseHeight, 0, 0, 4.0f);
+    perlinTex = LoadTextureFromImage(perlinImage);
 }
 
 // Gameplay Screen Update logic
 void UpdateGameplayScreen(void)
 {
-    // TODO: Update GAMEPLAY screen variables here!
+    framesCounter++;
+    perlinImage = GenImagePerlinNoise(noiseWidth, noiseHeight, framesCounter, 0, 4.0f);
+    UpdateTexture(perlinTex, perlinImage.data);
 
     // Press enter or tap to change to ENDING screen
-    if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
-    {
-        finishScreen = 1;
-        PlaySound(fxCoin);
-    }
+    // if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
+    // {
+    //     finishScreen = 1;
+    //     PlaySound(fxCoin);
+    // }
 }
 
 // Gameplay Screen Draw logic
 void DrawGameplayScreen(void)
 {
-    // TODO: Draw GAMEPLAY screen here!
-    DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), PURPLE);
-    Vector2 pos = { 20, 10 };
-    DrawTextEx(font, "GAMEPLAY SCREEN", pos, font.baseSize*3.0f, 4, MAROON);
-    DrawText("PRESS ENTER or TAP to JUMP to ENDING SCREEN", 130, 220, 20, MAROON);
+    DrawTexture(perlinTex, 0, noiseY, WHITE);
 }
 
 // Gameplay Screen Unload logic
 void UnloadGameplayScreen(void)
 {
-    // TODO: Unload GAMEPLAY screen variables here!
+    UnloadTexture(perlinTex);
+    UnloadImage(perlinImage);
 }
 
 // Gameplay Screen should finish?
