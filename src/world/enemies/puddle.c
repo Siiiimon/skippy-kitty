b062f969f0c49999252e4ab6a55dc4b824a0dd8f
Simon Cornelius <simonjoecornelius@gmail.com>
2025-02-25 14:45:16 +0100

    feat: add basic puddle

---
+++ src/world/enemies/puddle.c
@@ -0,0 +1,27 @@
+#include "puddle.h"
+#include "raylib.h"
+
+struct Puddle* NewPuddle(Vector2 position) {
+  struct Puddle* puddle = malloc(sizeof(struct Puddle));
+  struct Rectangle* dimensions = malloc(sizeof(Rectangle));
+  dimensions->x = position.x;
+  dimensions->y = position.y;
+  dimensions->width = PUDDLE_WIDTH;
+  dimensions->height = PUDDLE_HEIGHT;
+  puddle->dimensions = dimensions;
+  return puddle;
+}
+
+void UpdatePuddle(struct Puddle* puddle, float scroll) {
+  puddle->dimensions->x -= scroll;
+  return;
+}
+
+void DrawPuddle(struct Puddle* puddle) {
+  DrawRectangleLinesEx(*puddle->dimensions, 2.0f, RED);
+}
+
+void FreePuddle(struct Puddle* puddle) {
+  free(puddle->dimensions);
+  free(puddle);
+}
---
+++ src/world/enemies/puddle.h
@@ -0,0 +1,19 @@
+#ifndef PUDDLE_H
+#define PUDDLE_H
+
+#include "raylib.h"
+#include "stdlib.h"
+
+#define PUDDLE_WIDTH 20
+#define PUDDLE_HEIGHT 15
+
+struct Puddle {
+  Rectangle* dimensions;
+};
+
+struct Puddle* NewPuddle(Vector2 position);
+void UpdatePuddle(struct Puddle* puddle, float scroll);
+void DrawPuddle(struct Puddle* puddle);
+void FreePuddle(struct Puddle* puddle);
+
+#endif /* PUDDLE_H */
