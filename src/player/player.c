5531145cb5df456858868b44b4c242d726757a1d
Simon Cornelius <simonjoecornelius@gmail.com>
2025-02-25 01:06:45 +0100

    feat: simple physics-based jumping
    man that ground gen was so ass I had 7 fps wth

--- src/player/player.c
+++ src/player/player.c
@@ -3,19 +3,52 @@
 
 struct Player* NewPlayer(Vector2 position) {
   struct Player* player = malloc(sizeof(struct Player));
-  player->dimensions = CLITERAL(Rectangle){position.x-PLAYER_WIDTH, position.y-PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT};
+  Rectangle* dim = malloc(sizeof(Rectangle));
+  dim->x = position.x-PLAYER_WIDTH;
+  dim->y = position.y-PLAYER_HEIGHT;
+  dim->width = PLAYER_WIDTH;
+  dim->height = PLAYER_HEIGHT;
+  player->dimensions = dim;
+  player->jump_state = JUMP_STATE_GROUNDED;
+  player->velocity = 0;
+  player->acceleration = 0;
 
   return player;
 }
 
-void UpdatePlayer(struct Player* player) {
+void UpdatePlayer(struct Player* player, unsigned int ground_level) {
+  float delta_time = GetFrameTime();
+
+  if (player->jump_state == JUMP_STATE_JUMPING) {
+    player->acceleration += GRAVITY;
+  }
+
+  if (IsKeyPressed(KEY_SPACE) && player->jump_state == JUMP_STATE_GROUNDED) {
+    player->acceleration += JUMP_ACCELERATION;
+    player->jump_state = JUMP_STATE_JUMPING;
+  }
+
+  player->velocity += player->acceleration * delta_time;
+  player->dimensions->y -= player->velocity * delta_time;
+
+  if (player->dimensions->y >= ground_level-player->dimensions->height) {
+    player->dimensions->y = ground_level-player->dimensions->height;
+    player->acceleration = 0;
+    player->velocity = 0;
+    player->jump_state = JUMP_STATE_GROUNDED;
+  }
+      
   return;
 }
 
 void DrawPlayer(struct Player* player) {
-  DrawRectangleLinesEx(player->dimensions, 2.0f, CLITERAL(Color){0, 255, 0, 255});
+  DrawRectangleLinesEx(*player->dimensions, 2.0f, CLITERAL(Color){0, 255, 0, 255});
+  DebugText(TextFormat("acceleration: %f", player->acceleration));
+  DebugText(TextFormat("velocity: %f", player->velocity));
+  DebugText(TextFormat("Y position: %f", player->dimensions->y));
 }
 
 void FreePlayer(struct Player* player) {
+  free(player->dimensions);
   free(player);
 }
--- src/player/player.h
+++ src/player/player.h
@@ -1,18 +1,30 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include "../util.h"
 #include "raylib.h"
 #include "stdlib.h"
 
 #define PLAYER_WIDTH 50
 #define PLAYER_HEIGHT 80
 
+#define GRAVITY -70
+#define JUMP_ACCELERATION 1100
+
+enum JumpingState {
+  JUMP_STATE_GROUNDED,
+  JUMP_STATE_JUMPING,
+};
+
 struct Player {
-  Rectangle dimensions;
+  Rectangle* dimensions;
+  enum JumpingState jump_state;
+  float velocity;
+  float acceleration;
 };
 
 struct Player* NewPlayer(Vector2 position);
-void UpdatePlayer(struct Player* player);
+void UpdatePlayer(struct Player* player, unsigned int ground_level);
 void DrawPlayer(struct Player* player);
 void FreePlayer(struct Player* player);
 
